6fbabb4c17ecccf7ef5f57858a241cf17276cb31
Nikhil Dhengle <nikhildhengl123@gmail.com>
2023-12-06 01:51:40 -0500

    trying to run actual examples

--- js_assignments/week-1/offline-class-1/level-2/01-Class.js
+++ js_assignments/week-1/offline-class-1/level-2/01-Class.js
@@ -1,12 +1,15 @@
-
 class Animal {
   constructor(name, legCount) {
-    this.name = name
-    this.legCount = legCount
+    this.name = name;
+    this.legCount = legCount;
   }
   describe() {
-    return `${this.name} has ${this.legCount} legs`
+    return `${this.name} has ${this.legCount} legs`;
   }
 }
 
+// Create an instance of the Animal class
+let myAnimal = new Animal("animal", 10);
 
+// Call the describe method on the instance
+console.log(myAnimal.describe());
