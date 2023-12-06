36de245c16f2f6e265fe1c3ff0c81807ecb78ec4
Nikhil Dhengle <nikhildhengl123@gmail.com>
2023-12-06 01:55:27 -0500

    adding age in the class constructor

--- js_assignments/week-1/offline-class-1/level-2/01-Class.js
+++ js_assignments/week-1/offline-class-1/level-2/01-Class.js
@@ -1,15 +1,16 @@
 class Animal {
-  constructor(name, legCount) {
+  constructor(name, legCount, age) {
     this.name = name;
     this.legCount = legCount;
+    this.age = age;
   }
   describe() {
-    return `${this.name} has ${this.legCount} legs`;
+    return `${this.name} has ${this.legCount} legs and is ${this.age} years old`;
   }
 }
 
 // Create an instance of the Animal class
-let myAnimal = new Animal("animal", 10);
+let myAnimal = new Animal("My Dog", 4, 12);
 
 // Call the describe method on the instance
 console.log(myAnimal.describe());
